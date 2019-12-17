005ebf2c82d2f13674270a1593e4ce22944cbe41
bkasteller <43139731+bkasteller@users.noreply.github.com>
2019-12-17 21:00:44 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,39 +6,77 @@ class Player
 {
 	public static void main(String args[]) 
 	{
+		Scanner in = new Scanner(System.in);
 		Map grille = new Map(3);
-		grille.GetAllKey();
+		IA ia = new IA();
+		grille.showGrille();
+		while (true) {
+			System.out.println("Entrez x : ");
+			int x = in.nextInt();
+			System.out.println("Entrez y : ");
+			int y = in.nextInt();
+			grille.setPoint(x, y, 1);
+			grille.showGrille();
+			ia.play(grille);
+			grille.showGrille();
+		}
 	}
 }
 
 class Map 
 {
-	private HashMap<Point, Boolean> grille = new HashMap<Point, Boolean>();
+	int taille;
+	private HashMap<String, Point> grille = new HashMap<String, Point>();
 	
 	public Map(int taille) 
+	{
+		this.taille = taille;
+		fileGrille();
+	}
+	
+	private void fileGrille()
 	{
 		for (int j = 1; j <= taille; j++)
 		{
 			for (int i = 1; i <= taille; i++)
 			{
-				grille.put(new Point(i,j), null);
+				Point p = new Point(i, j);
+				grille.put(p.myToString(), p);
 			}
 		}
 	}
 	
-	public void GetAllKey()
+	public HashMap<String, Point> getGrille() {
+		return grille;
+	}
+	
+	public void showGrille()
 	{
-		Set<Point> keys = grille.keySet();
-		for (Point i : keys)
+		for (int j = 1; j <= taille; j++)
 		{
-			System.out.println(i.toString() + " : " + grille.get(i));
+			for (int i = 1; i <= taille; i++)
+			{
+				System.out.print(grille.get("(" + i + ";" + j + ")").getValue() == 0 ? "." : grille.get("(" + i + ";" + j + ")").getValue() == 1 ? "x" : "o");
+			}
+			System.out.println();
 		}
+		System.out.println();
+	}
+	
+	public void setPoint(int x, int y, int b)
+	{
+		grille.get("(" + x + ";" + y + ")").setValue(b);
+	}
+	
+	public int getPoint(int x, int y)
+	{
+		return grille.get("(" + x + ";" + y + ")").getValue();
 	}
 }
 
 class Point 
 {
-	private int x, y;
+	private int x, y, value = 0;
 	
 	public Point(int x, int y) 
 	{
@@ -46,18 +84,78 @@ class Point
 		this.y = y;
 	}
 	
-	public int GetX() 
+	public int getX() 
 	{
 		return x;
 	}
 	
-	public int GetY()
+	public int getY()
 	{
 		return y;
 	}
 	
-	@Override
-    public String toString() { 
-        return "(" + GetX() + ";" + GetY() + ")"; 
+	public int getValue()
+	{
+		return value;
+	}
+	
+	public void setValue(int value)
+	{
+		this.value = value;
+	}
+	
+    public String myToString() { 
+        return "(" + getX() + ";" + getY() + ")"; 
     } 
+}
+
+class IA
+{
+	public void play(Map grille)
+	{
+		grille.setPoint(1, 1, 2);
+	}
+}
+
+class OneCase
+{	
+	private Map grille;
+	private int score, x, y;
+	
+	public OneCase(Map grille, int x, int y) 
+	{
+		this.grille = grille;
+		this.x = x;
+		this.y = y;
+		this.score = calculScore();
+	}
+	
+	public int getScore()
+	{
+		return score;
+	}
+	
+	private int calculScore()
+	{
+		int score = 0;
+		boolean full_line = true;
+		for (int x = 1; x <= 3 && full_line; x++)
+			if (x != this.x)
+				if ( grille.getPoint(x, this.y) == 1 )
+					full_line = false;
+		if(full_line){score++;}else{full_line = true;};
+		for (int y = 1; y <= 3 && full_line; y++)
+			if (y != this.y)
+				if ( grille.getPoint(this.x, y) == 1 )
+					full_line = false;
+		if ( this.x == this.y || (grille.getSize() - this.x + 1) == this.y ) // nous somme sur une diagonale
+		{
+			if(full_line){score++;}else{full_line = true;};
+			
+			if(full_line){score++;}else{full_line = true;};
+			
+			if(full_line){score++;};
+		}
+		return score;
+	}
 }
