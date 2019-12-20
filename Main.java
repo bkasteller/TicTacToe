dd63a8306f1a9f6f2f0c2a8efaa72dd5751f7ee3
bkasteller <43139731+bkasteller@users.noreply.github.com>
2019-12-20 22:07:14 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,13 +10,13 @@ class Player
 		Grille grille = new Grille();
 		IA robot = new IA();
 		while ( !grille.FinPartie() ) {
-			grille.AfficherGrille();
 			robot.Jouer(grille);
+			grille.AfficherGrille();
 			System.out.println("Entrez x : ");
 			int x = in.nextInt();
 			System.out.println("Entrez y : ");
 			int y = in.nextInt();
-			grille.Jouer(x, y, grille.Joueur());
+			grille.Jouer(x-1, y-1, grille.Joueur());
 		}
 	}
 }
@@ -121,19 +121,17 @@ class Grille
 	public boolean FinPartie()
 	{
 		for (int i = 0; i < GetTaille(); i++)
-			if ( VerifLigne(i) || VerifCol(i) )
+			if ( VerifLigne(i, 3) != 0 || VerifCol(i, 3) != 0 )
 				return true;
-		if ( VerifDiago1() || VerifDiago2() )
+		if ( VerifDiago1(3) != 0 || VerifDiago2(3) != 0 )
 			return true;
-		
 		for ( String i : Key() )
 			if ( GetCase(i).GetVal() == 0 )
 				return false;
-		
 		return true;
 	}
 	
-	public boolean VerifLigne(int y)
+	public int VerifLigne(int y, int n)
 	{
 		int robot = 0, joueur = 0;
 		for (int x = 0; x < GetTaille(); x++)
@@ -144,10 +142,14 @@ class Grille
 			else if ( val == Robot() )
 				robot++;
 		}
-		return robot == 3 || joueur == 3;
+		if ( robot == n )
+			return Robot();
+		if ( joueur == n )
+			return Joueur();
+		return 0;
 	}
 	
-	public boolean VerifCol(int x)
+	public int VerifCol(int x, int n)
 	{
 		int robot = 0, joueur = 0;
 		for (int y = 0; y < GetTaille(); y++)
@@ -158,18 +160,174 @@ class Grille
 			else if ( val == Robot() )
 				robot++;
 		}
-		return robot == 3 || joueur == 3;
+		if ( robot == n )
+			return Robot();
+		if ( joueur == n )
+			return Joueur();
+		return 0;
 	}
 	
+	public int VerifDiago1(int n)
+	{
+		int robot = 0, joueur = 0;
+		for (int i = 0; i < GetTaille(); i++)
+		{
+			int val = GetCase("("+i+";"+i+")").GetVal();
+			if ( val == Joueur() )
+				joueur++;
+			else if ( val == Robot() )
+				robot++;
+		}
+		if ( robot == n )
+			return Robot();
+		if ( joueur == n )
+			return Joueur();
+		return 0;
+	}
 	
+	public int VerifDiago2(int n)
+	{
+		int robot = 0, joueur = 0;
+		for (int i = 0; i < GetTaille(); i++)
+		{
+			int val = GetCase("("+i+";"+(GetTaille()-i-1)+")").GetVal();
+			if ( val == Joueur() )
+				joueur++;
+			else if ( val == Robot() )
+				robot++;
+		}
+		if ( robot == n )
+			return Robot();
+		if ( joueur == n )
+			return Joueur();
+		return 0;
+	}
 }
 
 class IA
 {
 	Grille grille;
+	private int max_val = -1000, x, y, profondeur = 9;
+	
 	public void Jouer(Grille grille)
 	{
 		this.grille = grille;
-		
+		Reflechir();
+		grille.Jouer(x, y, 2);
+		System.out.println(x+" "+y);
 	}
+	
+	private void Reflechir()
+	{
+		for ( String i : grille.Key() )
+		{
+			if ( grille.GetCase(i).GetVal() == 0 )
+			{
+				int x = grille.GetCase(i).GetX(), y = grille.GetCase(i).GetY();
+				grille.Jouer(x, y, grille.Robot());
+				int val = Min(profondeur);
+				if ( val >= max_val)
+				{
+					max_val = val;
+					this.x = x;
+					this.y = y;
+				}
+				grille.Annuler(x, y);
+			}
+		}
+	}
+	
+	private int Min(int profondeur)
+	{
+		if ( profondeur == 0 || grille.FinPartie() )
+			return Eval();
+		int min_val = 1000;
+		for ( String i : grille.Key() )
+		{
+			if ( grille.GetCase(i).GetVal() == 0 )
+			{
+				int x = grille.GetCase(i).GetX(), y = grille.GetCase(i).GetY();
+				grille.Jouer(x, y, grille.Joueur());
+				int val = Max(profondeur-1);
+				if ( val < min_val)
+					min_val = val;
+				grille.Annuler(x, y);
+			}
+		}
+		return min_val;
+	}
+	
+	private int Max(int profondeur)
+	{
+		if ( profondeur == 0 || grille.FinPartie() )
+			return Eval();
+		int max_val = -1000;
+		for ( String i : grille.Key() )
+		{
+			if ( grille.GetCase(i).GetVal() == 0 )
+			{
+				int x = grille.GetCase(i).GetX(), y = grille.GetCase(i).GetY();
+				grille.Jouer(x, y, grille.Robot());
+				int val = Min(profondeur-1);	
+				if ( val > max_val)
+					max_val = val;	
+				grille.Annuler(x, y);
+			}
+		}
+		return max_val;
+	}
+	
+	private int Eval()
+	{
+		int score = 0;
+		
+		for (int i = 0; i < grille.GetTaille(); i++)
+		{
+			if ( grille.VerifLigne(i, 2) == grille.Robot() )
+				score += 2;
+			else if ( grille.VerifLigne(i, 2) == grille.Joueur() )
+				score -= 2;
+			else
+				score -= 1;
+			if ( grille.VerifLigne(i, 3) == grille.Robot() )
+				score += 1000;
+			else if ( grille.VerifLigne(i, 3) == grille.Joueur() )
+				score -= 1000;
+			if ( grille.VerifCol(i, 2) == grille.Robot() )
+				score += 2;
+			else if ( grille.VerifCol(i, 2) == grille.Joueur() )
+				score -= 2;
+			else
+				score -= 1;
+			if ( grille.VerifCol(i, 3) == grille.Robot() )
+				score += 1000;
+			else if ( grille.VerifCol(i, 3) == grille.Joueur() )
+				score -= 1000;
+		}
+		if ( grille.VerifDiago1(2) == grille.Robot() )
+			score += 2;
+		else if ( grille.VerifDiago1(2) == grille.Joueur() )
+			score -= 2;
+		else
+			score -= 1;
+		if ( grille.VerifDiago1(3) == grille.Robot() )
+			score += 10000;
+		else if ( grille.VerifDiago1(3) == grille.Joueur() )
+			score -= 1000;
+		if ( grille.VerifDiago2(2) == grille.Robot() )
+			score += 2;
+		else if ( grille.VerifDiago2(2) == grille.Joueur() )
+			score -= 2;
+		else
+			score -= 1;
+		if ( grille.VerifDiago2(3) == grille.Robot() )
+			score += 1000;
+		else if ( grille.VerifDiago2(3) == grille.Joueur() )
+			score -= 1000;
+		return score;
+	}
+}
+}
+}
+}
 }
